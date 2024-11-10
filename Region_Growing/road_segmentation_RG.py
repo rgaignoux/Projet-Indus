d2a90951dc86f98f549bd7f4021c2ff5cac702c5
rgaignoux <103379612+rgaignoux@users.noreply.github.com>
2024-11-10 10:56:35 +0100

    Few changes on the parameters

--- Region_Growing/road_segmentation_RG.py
+++ Region_Growing/road_segmentation_RG.py
@@ -19,24 +19,28 @@ def get_points_central_axis(img):
 
     return points
 
+central_axis_path = sys.argv[1]
+road_path = sys.argv[2]
+threshold = int(sys.argv[3])
+
 ##########################################################
 # First Step : Mask the road image with the central axis #
 ##########################################################
 
+# Load the road image
+road = cv2.imread(road_path)
+road = scale_image(road)
+road = preprocess_image2(road, filter_size = 15, sigma = 150) # Preprocess the image
+
 # Load the central axis image
-central_axis = cv2.imread('images/axe_central.png', cv2.IMREAD_GRAYSCALE)
-central_axis = resize_image(central_axis)
+central_axis = cv2.imread(central_axis_path, cv2.IMREAD_GRAYSCALE)
+central_axis = resize_image(central_axis, road.shape[1], road.shape[0])
 central_axis = cv2.bitwise_not(central_axis)  # Invert the image
 
 # Convert the image to a binary boolean array to skeletonize it
 binary_bool = central_axis > 0
 skeleton = morphology.skeletonize(binary_bool)
 skeleton = np.uint8(skeleton) * 255
-
-# Load the road image
-road = cv2.imread('images/route.png')
-road = resize_image(road)
-road = preprocess_image2(road, filter_size=5) # Preprocess the image
 
 # Apply dilation to the skeleton image
 kernel = np.ones((35, 35), np.uint8)
@@ -49,18 +53,17 @@ img_preprocessed = cv2.bitwise_and(road, road, mask=dilated_mask)
 # Second Step : Apply region growing to segment the road #
 ##########################################################
 
-# Subsample the skeleton points to get the seeds
-points = get_points_central_axis(skeleton)
-seeds = points[::5] # Subsample every 10 point
+# Get the seeds from all the central axis points
+seeds = get_points_central_axis(skeleton)
 
 # Display the seeds on the road image
 img_display = img_preprocessed.copy()
+img_display = cv2.convertScaleAbs(img_display)
 for seed in seeds:
     cv2.circle(img_display, (seed[1], seed[0]), 2, (0, 255, 0), -1) # dumb column-major (col, row) format...
 display_image("Seeds", img_display)
 
 # Perform region growing
-threshold = int(sys.argv[1])
 segmented_image = region_growing(img_preprocessed, seeds, threshold)
 
 # Overlay the segmentation result on the original image
@@ -74,13 +77,17 @@ display_image("Segmentation Result", overlay)
 # Third Step : Post-process the segmentation result #
 #####################################################
 
+# Apply morphological opening to remove inexistent roads
+kernel = np.ones((3, 3), np.uint8)
+opening = cv2.morphologyEx(segmented_image, cv2.MORPH_OPEN, kernel, iterations=1)
+display_image("Opening", opening)
+
 # Apply morphological closing to fill the gaps
-kernel = np.ones((7, 7), np.uint8)
-closing = cv2.morphologyEx(segmented_image, cv2.MORPH_CLOSE, kernel, iterations=3)
+closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel, iterations=1)
 display_image("Closing", closing)
 
 # Smooth the edges by applying median filter
-smoothed = cv2.medianBlur(closing, 9)
+smoothed = cv2.medianBlur(closing, 3)
 display_image("Smoothed", smoothed)
 
 # Overlay the smoothed result on the original image

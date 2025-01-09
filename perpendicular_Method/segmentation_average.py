c23708e8a1afda3f2c77ed4697e3bf1a6c6728e3
rgaignoux <103379612+rgaignoux@users.noreply.github.com>
2025-01-09 19:22:46 +0100

    New results

--- perpendicular_Method/segmentation_average.py
+++ perpendicular_Method/segmentation_average.py
@@ -44,21 +44,22 @@ for (axis_path, road_path) in zip(axes_paths, road_paths):
 
     # Load the central axis image
     central_axis = cv2.imread(axis_path, cv2.IMREAD_GRAYSCALE)
+    central_axis = cv2.resize(central_axis, (road.shape[1], road.shape[0]))
     central_axis = cv2.bitwise_not(central_axis) # Invert the image
 
     # Filter out the white pixels
     hsv_image = cv2.cvtColor(road, cv2.COLOR_BGR2HSV)
-    lower_white = np.array([0, 0, 160])
-    upper_white = np.array([180, 60, 255])
+    lower_white = np.array([0, 0, 140])
+    upper_white = np.array([180, 40, 255])
     mask = cv2.inRange(hsv_image, lower_white, upper_white)
     hsv_image[mask > 0] = [0, 0, 160]
     road2 = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
 
     # Gaussian filtering
-    road_blurred = cv2.medianBlur(road2, 5)
+    road_blurred = cv2.bilateralFilter(road2, 9, 150, 150)
 
     # Canny edge detection
-    edges = cv2.Canny(road_blurred, 75, 75)
+    edges = cv2.Canny(road_blurred, 100, 125)
 
     # Find the road edges using normals
     normals, points = extract_normals(central_axis)
@@ -104,7 +105,7 @@ for (axis_path, road_path) in zip(axes_paths, road_paths):
 
     for index, pos in enumerate(points):
         # Extract 2*k points around the current point
-        k = 75
+        k = 100
         (i, j) = pos
         start = index - k
         end = index + k
@@ -178,7 +179,9 @@ for (axis_path, road_path) in zip(axes_paths, road_paths):
 
     # Overlay the segmentation on the road image
     result = road.copy()
+    result[central_axis >= 1] = np.array([0, 0, 0])
     result[segmentation_mask >= 1] = (0.4 * np.array([0, 255, 255]) + 0.6 * result[segmentation_mask >= 1]).astype(np.uint8)
+    
     cv2.imwrite(f"perpendicular_Method//results//overlay_{filename}.png", result)
     if display == 1:
         utils.display_image("Result", result)

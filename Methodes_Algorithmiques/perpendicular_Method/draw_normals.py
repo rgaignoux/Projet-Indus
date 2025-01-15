96f78740ab6f61d5377f99ad35f6b8401b060aa8
rgaignoux <103379612+rgaignoux@users.noreply.github.com>
2025-01-15 18:40:41 +0100

    Go back to old normal computing

--- Methodes_Algorithmiques/perpendicular_Method/draw_normals.py
+++ Methodes_Algorithmiques/perpendicular_Method/draw_normals.py
@@ -5,7 +5,7 @@ import argparse
 import utils
 import math
 
-""" def correct_normal_orientation(normals):
+def correct_normal_orientation(normals):
     for idx in range(1, len(normals)):
         _, (prev_norm_x, prev_norm_y) = normals[idx - 1]
         (i, j), (curr_norm_x, curr_norm_y) = normals[idx]
@@ -17,17 +17,17 @@ import math
         if dot_product < 0:
             normals[idx] = ((i, j), (-curr_norm_x, -curr_norm_y))
 
-    return normals """
+    return normals
 
-""" def angle_between_vectors(v1, v2):
+def angle_between_vectors(v1, v2):
     dot_product = v1[0] * v2[0] + v1[1] * v2[1]
     magnitude_v1 = np.sqrt(v1[0]**2 + v1[1]**2)
     magnitude_v2 = np.sqrt(v2[0]**2 + v2[1]**2)
     cos_theta = dot_product / (magnitude_v1 * magnitude_v2 + 1e-8)
-    return np.arccos(np.clip(cos_theta, -1, 1)) """
+    return np.arccos(np.clip(cos_theta, -1, 1))
 
+def remove_normals_outliers(normals, angle_threshold = np.pi / 10):
 
-""" def remove_normals_outliers(normals, angle_threshold = np.pi / 10):
     corrected_normals = []
 
     for idx in range(len(normals)):
@@ -57,62 +57,23 @@ import math
                 corrected_normals.append(((i, j), (norm_x, norm_y)))
         
         else:
-            corrected_normals.append(((i, j), (norm_x, norm_y))) """
+            corrected_normals.append(((i, j), (norm_x, norm_y)))
 
-def compute_average_normals(normals, neighbor_count=5):
-    average_normals = []
-
-    for idx in range(len(normals)):
-        (i, j), (norm_x, norm_y) = normals[idx]
-
-        if np.isnan(norm_x) or np.isnan(norm_y):
-            norm_x, norm_y = 0, 0
-
-        # Extract the neighbor normals (10 previous and next ones)
-        neighbor_normals = []
-        for offset in range(-neighbor_count, neighbor_count + 1):
-            if offset == 0:
-                continue
-            neighbor_idx = idx + offset
-            if 0 <= neighbor_idx < len(normals):
-                _, (neighbor_x, neighbor_y) = normals[neighbor_idx]
-
-                if np.isnan(neighbor_x) or np.isnan(neighbor_y):
-                    neighbor_x, neighbor_y = 0, 0
-
-                neighbor_normals.append((neighbor_x, neighbor_y))
-
-        # Compute average normal from neighbors
-        if neighbor_normals:
-            avg_x = np.mean([n[0] for n in neighbor_normals])
-            avg_y = np.mean([n[1] for n in neighbor_normals])
-            avg_magnitude = np.sqrt(avg_x**2 + avg_y**2)
-            avg_normal = (avg_x / avg_magnitude, avg_y / avg_magnitude)  # Normalize the average normal
-
-            if np.isnan(avg_normal[0]) or np.isnan(avg_normal[1]):
-                avg_normal = (0, 0)
-
-            # Replace the current normal with the average normal
-            average_normals.append(((i, j), avg_normal))
-        else:
-            average_normals.append(((i, j), (norm_x, norm_y)))
-
-    return average_normals
-
+    return corrected_normals
 
 def extract_normals(central_axis):
     # Skeletonize the central axis image
-    _, points = utils.skeletonize_image(central_axis)
+    skeleton, points = utils.skeletonize_image(central_axis)
     points = utils.sort_points(points)
 
     # Sobel filter to compute the gradient
-    sobel_x = cv2.Sobel(central_axis, cv2.CV_64F, 1, 0, ksize=7)
-    sobel_y = cv2.Sobel(central_axis, cv2.CV_64F, 0, 1, ksize=7)
+    sobel_x = cv2.Sobel(central_axis, cv2.CV_64F, 1, 0, ksize=5)
+    sobel_y = cv2.Sobel(central_axis, cv2.CV_64F, 0, 1, ksize=5)
 
     # Compute normals
     normals = []
     reference_normal = None  # Initial reference normal
-    update_interval = 50
+    update_interval = 100
 
     for k in range(0, len(points)):
         (i, j) = points[k]
@@ -140,13 +101,13 @@ def extract_normals(central_axis):
         else:
             normals.append(((i, j), (0, 0)))
 
-    # Make the normal more robust by using neighbors average
-    average_normals = compute_average_normals(normals)
+    # Remove "outliers"
+    normals = remove_normals_outliers(normals)
 
     # Transform to dict
-    normals_dict = {key: value for key, value in average_normals}
+    normals_dict = {key: value for key, value in normals}
 
-    return normals_dict, points
+    return normals_dict, points, skeleton
 
 if __name__ == "__main__":
     # Parse the arguments
@@ -163,11 +124,11 @@ if __name__ == "__main__":
     skeleton_rgb = cv2.cvtColor(skeleton, cv2.COLOR_GRAY2BGR)
 
     # Compute normals
-    normals, _ = extract_normals(central_axis)
+    normals, _, _ = extract_normals(central_axis)
 
     line_length = 10
     for (i, j), (norm_x, norm_y) in normals.items():
-        if not(norm_x == 0 and norm_y == 0):
+        if norm_x != 0 or norm_y != 0:
             x1 = j
             y1 = i
             x2 = int(j + line_length * norm_x)
